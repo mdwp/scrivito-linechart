55985bb0ac91ec3ebee336661cff6271280cea43
Meinolf Droste <mdwp.post@gmail.com>
2019-04-13 15:33:37 +0200

    scrivito placeholder added

--- src/LineChartWidgetComponent.js
+++ src/LineChartWidgetComponent.js
@@ -15,16 +15,13 @@ Scrivito.provideComponent("LineChartWidget", ({ widget }) => {
       return obj;
     });
 
-
-
-
-    /*if (!items.length) {
+    if (!items.length && Scrivito.isInPlaceEditingActive()) {
       return (
-        <InPlaceEditingPlaceholder center={ true }>
-          Set Line chart values in the widget properties.
-        </InPlaceEditingPlaceholder>
+          <h4 className="text-center">
+            Provide the linechart values in the widget properties.
+          </h4>
       );
-    }*/
+    }
 
     return (
       type === 'dashed' ?
